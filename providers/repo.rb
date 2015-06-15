b95aadf29dcb27615d4ee327bddcada1e17df6a5
Kimball Johnson <kj@chef.io>
2015-06-15 16:00:22 +0100

    Add why_run support to permissions changes

--- providers/repo.rb
+++ providers/repo.rb
@@ -17,7 +17,6 @@ action :create do
       create(server, user, repo_opts)
     end
   end
-  Chef::Log.debug("New Resource : #{@new_resource}")
   update_perms(server, user, repo_opts, "groups", "REPO_ADMIN", @current_resource.admin_groups, @new_resource.admin_groups)
   update_perms(server, user, repo_opts, "groups", "REPO_WRITE", @current_resource.write_groups, @new_resource.write_groups)
   update_perms(server, user, repo_opts, "groups", "REPO_READ", @current_resource.read_groups, @new_resource.read_groups)
@@ -47,15 +46,6 @@ action :delete do
 end
 
 def load_current_resource
-
-  Chef::Log.debug("Loading current stash_repo resource: #{@new_resource.repo} New Resource Object ID: #{@new_resource.object_id}")
-  Chef::Log.debug("New Resource permissions:")
-  Chef::Log.debug("Admin Groups: #{@new_resource.admin_groups}")
-  Chef::Log.debug("Write Groups: #{@new_resource.write_groups}")
-  Chef::Log.debug("Read Groups: #{@new_resource.read_groups}")
-  Chef::Log.debug("Admin Users: #{@new_resource.admin_users}")
-  Chef::Log.debug("Write Users: #{@new_resource.write_users}")
-  Chef::Log.debug("Read Users: #{@new_resource.read_users}")
   server = @new_resource.server
   user = @new_resource.user
   repo_opts = {
@@ -67,22 +57,6 @@ def load_current_resource
   install_chef_vault(@new_resource.chef_vault_source, @new_resource.chef_vault_version)
 
   @current_resource = Chef::Resource::StashRepo.new(repo_opts['repo'])
-
-  Chef::Log.debug("New Resource permissions (right after current_resource created:")
-  Chef::Log.debug("Admin Groups: #{@new_resource.admin_groups}")
-  Chef::Log.debug("Write Groups: #{@new_resource.write_groups}")
-  Chef::Log.debug("Read Groups: #{@new_resource.read_groups}")
-  Chef::Log.debug("Admin Users: #{@new_resource.admin_users}")
-  Chef::Log.debug("Write Users: #{@new_resource.write_users}")
-  Chef::Log.debug("Read Users: #{@new_resource.read_users}")
-  Chef::Log.debug("Current resource Object ID: #{@current_resource.object_id}")
-    Chef::Log.debug("Admin Groups: #{@current_resource.admin_groups}")
-  Chef::Log.debug("Write Groups: #{@current_resource.write_groups}")
-  Chef::Log.debug("Read Groups: #{@current_resource.read_groups}")
-  Chef::Log.debug("Admin Users: #{@current_resource.admin_users}")
-  Chef::Log.debug("Write Users: #{@current_resource.write_users}")
-  Chef::Log.debug("Read Users: #{@current_resource.read_users}")
-
   @current_resource.exists = exists?(server, user, repo_opts) 
 
   # Load in existing permissions if the repo already exists
@@ -114,13 +88,6 @@ def load_current_resource
     end
   end
 
-  Chef::Log.debug("New Resource permissions (end of load_current_resource:")
-  Chef::Log.debug("Admin Groups: #{@new_resource.admin_groups}")
-  Chef::Log.debug("Write Groups: #{@new_resource.write_groups}")
-  Chef::Log.debug("Read Groups: #{@new_resource.read_groups}")
-  Chef::Log.debug("Admin Users: #{@new_resource.admin_users}")
-  Chef::Log.debug("Write Users: #{@new_resource.write_users}")
-  Chef::Log.debug("Read Users: #{@new_resource.read_users}")
 end
 
 private
@@ -158,22 +125,21 @@ end
 def update_perms(server, user, repo_opts, type, permission, current_list, new_list)
   to_add = new_list - current_list
   to_remove = current_list - new_list
-  Chef::Log.debug "Repo: #{repo_opts['repo']} Type: #{type} Permission: #{permission}"
-  Chef::Log.debug "Current List: #{current_list.to_s}"
-  Chef::Log.debug "New List: #{new_list.to_s}"
-  Chef::Log.debug "Adding: #{to_add.to_s}"
-  Chef::Log.debug "Removing: #{to_remove.to_s}"
   base_uri = "projects/#{repo_opts['project']}/repos/#{repo_opts['repo']}/permissions/#{type}"
   to_add.each do |item|
-    uri = stash_uri(server, "#{base_uri}?permission=#{permission}&name=#{item}")
-    Chef::Log.debug "Stash Request: PUT |#{uri.request_uri}|"
-    stash_put(uri, user, nil, ['204'])
+    converge_by(" Adding #{permission} to #{type} #{item} on repo #{repo_opts['repo']} in project #{repo_opts['project']}") do
+      uri = stash_uri(server, "#{base_uri}?permission=#{permission}&name=#{item}")
+      Chef::Log.debug "Stash Request: PUT |#{uri.request_uri}|"
+      stash_put(uri, user, nil, ['204'])
+    end 
   end
 
   to_remove.each do |item|
-    uri = stash_uri(server, "#{base_uri}?name=#{item}")
-    Chef::Log.debug "Stash Request: DELETE |#{uri.request_uri}|"
-    stash_delete(uri, user, ['204'])
+    converge_by("Removing #{permission} to #{type} #{item} on repo #{repo_opts['repo']} in project #{repo_opts['project']}") do
+      uri = stash_uri(server, "#{base_uri}?name=#{item}")
+      Chef::Log.debug "Stash Request: DELETE |#{uri.request_uri}|"
+      stash_delete(uri, user, ['204'])
+    end
   end
 end
 
