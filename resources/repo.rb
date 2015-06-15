036c2e7934f0e86bddf815314580f246c69ede6d
Kimball Johnson <kj@chef.io>
2015-06-15 14:49:42 +0100

    Ensure arrays in current_resource are created newly

--- resources/repo.rb
+++ resources/repo.rb
@@ -8,11 +8,11 @@ attribute :user, :kind_of => String
 attribute :chef_vault_version, :kind_of => String, :default => '1.2.0'
 attribute :chef_vault_source, :kind_of => String, :default => nil
 
-#attribute :read_groups, :kind_of => Array
-#attribute :write_groups, :kind_of => Array
-#attribute :admin_groups, :kind_of => Array
-#attribute :read_users, :kind_of => Array
-#attribute :write_users, :kind_of => Array
-#attribute :admin_users, :kind_of => Array
+attribute :read_groups, :kind_of => Array
+attribute :write_groups, :kind_of => Array
+attribute :admin_groups, :kind_of => Array
+attribute :read_users, :kind_of => Array
+attribute :write_users, :kind_of => Array
+attribute :admin_users, :kind_of => Array
 
-attr_accessor :exists, :read_groups, :write_groups, :admin_groups, :read_users, :write_users, :admin_users
+attr_accessor :exists
